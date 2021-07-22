9be560b3027a887a91e6afb9b320fba4a9c530dd
hyunmin0317 <choihm9903@naver.com>
2021-07-22 19:42:29 +0900

    Delete

--- covid19/models.py
+++
@@ -1,8 +0,0 @@
-class Covid19():
-    def __init__(self, date, confirmed, death, released, tested, today):
-        self.date = date
-        self.confirmed = confirmed
-        self.death = death
-        self.released = released
-        self.tested = tested
-        self.today = today
